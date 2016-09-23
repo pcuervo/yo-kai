23b0f01d970158a93028cf878580948e688339b8
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-09-23 16:51:26 -0500

    estilo categorias en album

--- themes/yo-kai/inc/pages.php
+++ themes/yo-kai/inc/pages.php
@@ -124,4 +124,16 @@ add_action('init', function(){
 		wp_insert_post( $page, true );
 	}
 
+	// Album
+	if( ! get_page_by_path('album') ){
+		$page = array(
+			'post_author' => 1,
+			'post_status' => 'publish',
+			'post_title'  => 'Album',
+			'post_name'   => 'album',
+			'post_type'   => 'page'
+		);
+		wp_insert_post( $page, true );
+	}
+
 });
--- themes/yo-kai/page-album.php
+++ themes/yo-kai/page-album.php
@@ -1,10 +1,21 @@
 <?php get_header(); ?>
 	<div class="[ width--800p ][ margin-auto ]">
 		<div class="row [ margin-bottom--large ]">
-			<div class="col-sm-4">
-
+			<div class="col-xs-4">
+				<div class="[ portrait-perfil ]">
+					<img class="[ portrait ]" src="<?php echo THEMEPATH; ?>images/portrait.png" alt="portada perfil">
+					<img class="[ avatar ]" src="<?php echo THEMEPATH; ?>images/perfil/whisper.png" alt="portada perfil">
+					<p class="[ nombre ]">Juancho panza</p>
+					<p class="[ puntaje ]">44/44</p>
+					<div class="[ border ]">
+						<div class="[ circle ]">
+							<p>Nº</p>
+							<p>888</p>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="col-sm-8">
+			<div class="col-xs-8">
 
 			</div>
 		</div>
@@ -13,8 +24,65 @@
 				<a href="#" class="[ inline-block ][ btn btn--primary ][ text-center ][ margin-auto ]">Cargar</a>
 			</div>
 		</div>
-		<div>
-			<a href=""></a>
+		<div class="[ absolute bottom--48 ]">
+			<div class="[ text-center ]">
+				<div class="[ category-card ]">
+					<a href="#">
+						<img class="[ img-initial ]" src="<?php echo THEMEPATH; ?>/icons/icon_0.png" alt="icono categoría carta">
+						<img class="[ img-hover ]" src="<?php echo THEMEPATH; ?>/icons/icon_0a.png" alt="icono categoría carta">
+					</a>
+				</div>
+				<div class="[ category-card ]">
+					<a href="#">
+						<img class="[ img-initial ]" src="<?php echo THEMEPATH; ?>/icons/icon_1.png" alt="icono categoría carta">
+						<img class="[ img-hover ]" src="<?php echo THEMEPATH; ?>/icons/icon_1a.png" alt="icono categoría carta">
+					</a>
+				</div>
+				<div class="[ category-card ]">
+					<a href="#">
+						<img class="[ img-initial ]" src="<?php echo THEMEPATH; ?>/icons/icon_2.png" alt="icono categoría carta">
+						<img class="[ img-hover ]" src="<?php echo THEMEPATH; ?>/icons/icon_2a.png" alt="icono categoría carta">
+					</a>
+				</div>
+				<div class="[ category-card ]">
+					<a href="#">
+						<img class="[ img-initial ]" src="<?php echo THEMEPATH; ?>/icons/icon_3.png" alt="icono categoría carta">
+						<img class="[ img-hover ]" src="<?php echo THEMEPATH; ?>/icons/icon_3a.png" alt="icono categoría carta">
+					</a>
+				</div>
+				<div class="[ category-card ]">
+					<a href="#">
+						<img class="[ img-initial ]" src="<?php echo THEMEPATH; ?>/icons/icon_4.png" alt="icono categoría carta">
+						<img class="[ img-hover ]" src="<?php echo THEMEPATH; ?>/icons/icon_4a.png" alt="icono categoría carta">
+					</a>
+				</div>
+				<div class="[ category-card ]">
+					<a href="#">
+						<img class="[ img-initial ]" src="<?php echo THEMEPATH; ?>/icons/icon_5.png" alt="icono categoría carta">
+						<img class="[ img-hover ]" src="<?php echo THEMEPATH; ?>/icons/icon_5a.png" alt="icono categoría carta">
+					</a>
+				</div>
+				<div class="[ category-card ]">
+					<a href="#">
+						<img class="[ img-initial ]" src="<?php echo THEMEPATH; ?>/icons/icon_6.png" alt="icono categoría carta">
+						<img class="[ img-hover ]" src="<?php echo THEMEPATH; ?>/icons/icon_6a.png" alt="icono categoría carta">
+					</a>
+				</div>
+				<div class="[ category-card ]">
+					<a href="#">
+						<img class="[ img-initial ]" src="<?php echo THEMEPATH; ?>/icons/icon_7.png" alt="icono categoría carta">
+						<img class="[ img-hover ]" src="<?php echo THEMEPATH; ?>/icons/icon_7a.png" alt="icono categoría carta">
+					</a>
+				</div>
+				<div class="[ category-card ]">
+					<a href="#">
+						<img class="[ img-initial ]" src="<?php echo THEMEPATH; ?>/icons/icon_8.png" alt="icono categoría carta">
+						<img class="[ img-hover ]" src="<?php echo THEMEPATH; ?>/icons/icon_8a.png" alt="icono categoría carta">
+					</a>
+				</div>
+			</div>
 		</div>
+
+
 	</div>
 <?php get_footer(); ?>
