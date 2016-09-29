58977b91e22ae7f0ce0f07b9d664787be7a2a9cb
alejandro <alejandro_mennn@hotmail.com>
2016-09-29 18:25:54 -0500

    posicion videos

--- themes/yo-kai/page-videos.php
+++ themes/yo-kai/page-videos.php
@@ -10,26 +10,61 @@
 		</div>
 	</div>
 	<div class="row [ margin-bottom--large ]">
-		<div class="col-xs-12">
-			<div class="embed-responsive embed-responsive-16by9">
-				<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/z_P2Uwfyppo" frameborder="0" allowfullscreen></iframe>
+		<?php $video_1 = get_option( 'orden_videos_1');
+		if ($video_1 != ''): 
+			$id_video = get_post_meta( $video_1, 'id_video', true ); ?>
+			<div class="col-xs-12">
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?php echo $id_video; ?>" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<h3 class="[ color-light ][ text-uppercase ]"><?php echo get_the_title($video_1); ?></h3>
 			</div>
-			<h3 class="[ color-light ][ text-uppercase ]">Nombre del video 1</h3>
-		</div>
+		<?php endif; ?>
 	</div>
 	<div class="row [ margin-bottom--large ]">
-		<div class="col-xs-6">
-			<div class="embed-responsive embed-responsive-16by9">
-				<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/-YcBkG8rdEg" frameborder="0" allowfullscreen></iframe>
+		<?php $video_2 = get_option( 'orden_videos_2');
+		if ($video_2 != ''): 
+			$id_video_2 = get_post_meta( $video_2, 'id_video', true ); ?>
+			<div class="col-xs-6">
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?php echo $id_video_2; ?>" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<h3 class="[ color-light ][ text-uppercase ]"><?php echo get_the_title($video_2); ?></h3>
 			</div>
-			<h3 class="[ color-light ][ text-uppercase ]">Nombre del video 2</h3>
-		</div>
-		<div class="col-xs-6">
-			<div class="embed-responsive embed-responsive-16by9">
-				<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/bcOzeLHLMw8" frameborder="0" allowfullscreen></iframe>			</div>
-			<h3 class="[ color-light ][ text-uppercase ]">Nombre del video 3</h3>
-		</div>
+		<?php endif;
+		$video_3 = get_option( 'orden_videos_3');
+		if ($video_3 != ''): 
+			$id_video_3 = get_post_meta( $video_3, 'id_video', true ); ?>
+			<div class="col-xs-6">
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?php echo $id_video_3; ?>" frameborder="0" allowfullscreen></iframe>			</div>
+				<h3 class="[ color-light ][ text-uppercase ]"><?php echo get_the_title($video_3); ?></h3>
+			</div>
+		<?php endif; ?>
 	</div>
+	<?php $video_4 = get_option( 'orden_videos_4');
+	$video_5 = get_option( 'orden_videos_5');
+	if ($video_4 || $video_5): ?>
+		<div class="row [ margin-bottom--large ]">
+			<?php if ($video_4 != ''): 
+				$id_video_4 = get_post_meta( $video_4, 'id_video', true ); ?>
+				<div class="col-xs-6">
+					<div class="embed-responsive embed-responsive-16by9">
+						<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?php echo $id_video_4; ?>" frameborder="0" allowfullscreen></iframe>
+					</div>
+					<h3 class="[ color-light ][ text-uppercase ]"><?php echo get_the_title($video_4); ?></h3>
+				</div>
+			<?php endif;
+			if ($video_5 != ''): 
+				$id_video_5 = get_post_meta( $video_5, 'id_video', true ); ?>
+				<div class="col-xs-6">
+					<div class="embed-responsive embed-responsive-16by9">
+						<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?php echo $id_video_5; ?>" frameborder="0" allowfullscreen></iframe>			</div>
+					<h3 class="[ color-light ][ text-uppercase ]"><?php echo get_the_title($video_5); ?></h3>
+				</div>
+			<?php endif; ?>
+		</div>
+	<?php endif; ?>
 	<div class="row [ margin-bottom--xxlarge ]">
 		<div class="col-xs-6">
 			<a href="http://www.hasbro.com/es-mx/toys-games/yokai" target="_blank" class="[ text-decoration-none block ]">
