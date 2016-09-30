963a480e8b6a6ad26f0ec7b059412672f088b1b2
alejandro <alejandro_mennn@hotmail.com>
2016-09-30 18:57:31 -0500

    cambio mensaje de exito ayuda

--- themes/yo-kai/inc/mails.class.php
+++ themes/yo-kai/inc/mails.class.php
@@ -117,7 +117,7 @@ class Mails{
 		//SEND EMAIL CONFIRMATION
 		$resp = wp_mail( 'raul@pcuervo.com', $subject, $message, $headers );
 		if ($resp) {
-			$success = 'Se enviaron con exito su nickname y contraseña';
+			$success = 'Se envio tu consulta correctamente';
 		}
 	}
 
