e73d3ada09b69c4660be37914188041a454a603a
alejandro <alejandro_mennn@hotmail.com>
2016-09-30 21:25:07 -0500

    orden descargables

--- themes/yo-kai/page-descargables.php
+++ themes/yo-kai/page-descargables.php
@@ -14,18 +14,18 @@ global $current_user;
 	</div>
 	<div class="[ row ][ text-center ]">
 		<div class="[ col-xs-4 ]">
-			<?php $desc_3 = get_option( 'descargable_3' );
-			if ($countMedallasParticipante >= 30): ?>
-				<img src="<?php echo THEMEPATH; ?>images/puerta1a.png" alt="imagen de descargable">
+			<?php $desc_1 = get_option( 'descargable_1' );
+			if ($countMedallasParticipante >= 10): ?>
+				<img src="<?php echo THEMEPATH; ?>images/puerta3a.png" alt="imagen de descargable">
 			<?php else: ?>
-				<img src="<?php echo THEMEPATH; ?>images/puerta1.png" alt="imagen de descargable">
+				<img src="<?php echo THEMEPATH; ?>images/puerta3.png" alt="imagen de descargable">
 			<?php endif; ?>
 			<div class="[ text-center ][ margin-bottom--xxlarge ]">
 				<div class="[ border-primary border-radius--20 ][ inline-block ]">
-					<?php if ($countMedallasParticipante >= 30): ?>
-						<a href="<?php echo $desc_3; ?>" target="_blank" class="[ inline-block ][ btn btn--primary ][ text-center ][ margin-auto ][ width--200p ]">DESCARGAR</a>
+					<?php if ($countMedallasParticipante >= 10): ?>
+						<a href="<?php echo $desc_1; ?>" target="_blank" class="[ inline-block ][ btn btn--primary ][ text-center ][ margin-auto ][ width--200p ]">DESCARGAR</a>
 					<?php else: ?>
-						<a href="#" class="[ inline-block ][ btn btn--primary ][ text-center ][ margin-auto ][ width--200p ]">30/44</a>
+						<a href="#" class="[ inline-block ][ btn btn--primary ][ text-center ][ margin-auto ][ width--200p ]">10/44</a>
 					<?php endif; ?>
 				</div>
 			</div>
@@ -48,22 +48,23 @@ global $current_user;
 			</div>
 		</div>
 		<div class="[ col-xs-4 ]">
-			<?php $desc_1 = get_option( 'descargable_1' );
-			if ($countMedallasParticipante >= 10): ?>
-				<img src="<?php echo THEMEPATH; ?>images/puerta3a.png" alt="imagen de descargable">
+			<?php $desc_3 = get_option( 'descargable_3' );
+			if ($countMedallasParticipante >= 30): ?>
+				<img src="<?php echo THEMEPATH; ?>images/puerta1a.png" alt="imagen de descargable">
 			<?php else: ?>
-				<img src="<?php echo THEMEPATH; ?>images/puerta3.png" alt="imagen de descargable">
+				<img src="<?php echo THEMEPATH; ?>images/puerta1.png" alt="imagen de descargable">
 			<?php endif; ?>
 			<div class="[ text-center ][ margin-bottom--xxlarge ]">
 				<div class="[ border-primary border-radius--20 ][ inline-block ]">
-					<?php if ($countMedallasParticipante >= 10): ?>
-						<a href="<?php echo $desc_1; ?>" target="_blank" class="[ inline-block ][ btn btn--primary ][ text-center ][ margin-auto ][ width--200p ]">DESCARGAR</a>
+					<?php if ($countMedallasParticipante >= 30): ?>
+						<a href="<?php echo $desc_3; ?>" target="_blank" class="[ inline-block ][ btn btn--primary ][ text-center ][ margin-auto ][ width--200p ]">DESCARGAR</a>
 					<?php else: ?>
-						<a href="#" class="[ inline-block ][ btn btn--primary ][ text-center ][ margin-auto ][ width--200p ]">10/44</a>
+						<a href="#" class="[ inline-block ][ btn btn--primary ][ text-center ][ margin-auto ][ width--200p ]">30/44</a>
 					<?php endif; ?>
 				</div>
 			</div>
 		</div>
+		
 	</div>
 
 </div>
